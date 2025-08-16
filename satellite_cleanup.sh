8908335356d4fe71e9ec9566b0fc472273a438cf
Javier Blanco <javier@jblanco.es>
2025-08-16 10:42:11 +0200

    Minor cosmetic changes in satellite_cleanup.sh

--- satellite_cleanup.sh
+++ satellite_cleanup.sh
@@ -12,7 +12,6 @@
 howto()
 {
    # Display Help
-   echo "Script to cleanup tasks/audits/reports in satellite."
    echo
    echo "Syntax: $0 <number-of-days-to-keep>"
    echo
@@ -21,7 +20,7 @@ howto()
 # check parameter
 
 if [ $# -ne 1 ]; then
-  printf "\n[ERR] This script requires one parameter to work.\n\n"
+  printf "Error. This script requires one parameter to work\n"
   howto
   exit 1
 fi
@@ -31,7 +30,7 @@ dtok=$1
 [ "$dtok" -eq "$dtok" ] 2>/dev/null
 
 if [ $? -ne 0 ]; then
-  printf "\n[ERR] Parameter must be a number.\n\n"
+  printf "Error. Parameter must be a number\n"
   howto
   exit 1
 fi
@@ -43,26 +42,26 @@ tnit="d"
 which foreman-rake 2>/dev/null
 
 if [ $? -ne 0 ]; then
-  printf "\n[ERR] foreman-rake seems not to be installed. Is Satellite installed here?\n\n"
+  printf "Error. Utility foreman-rake seems not to be installed. Is Satellite installed here?\n"
   exit 1
 fi
 
 foreman-rake audits:expire days=$dtok
 
 if [ $? -ne 0 ]; then
-  printf "\n[ERR] Audit cleanup failed.\n\n"
+  printf "Error. Audit cleanup failed\n"
 fi
 
 foreman-rake reports:expire days=$dtok
 
 if [ $? -ne 0 ]; then
-  printf "\n[ERR] Reports cleanup failed.\n\n"
+  printf "Error. Reports cleanup failed\n"
 fi
 
 foreman-rake foreman_tasks:cleanup TASK_SEARCH='label ~ *' AFTER="$dtok$tnit"
 
 if [ $? -ne 0 ]; then
-  printf "\n[ERR] Tasks cleanup failed.\n\n"
+  printf "Error. Tasks cleanup failed\n"
 fi
 
 exit
